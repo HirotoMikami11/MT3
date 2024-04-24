ec4dda169a86011445e46b065c6381b830a2ceac
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2024-04-24 13:11:55 +0900

    一時保存

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <MyMath.h>
+#include <ImGuiManager.h>
 
 const char kWindowTitle[] = "LE1A_16_ミカミ_ヒロト_MT3_02_00";
 
@@ -34,11 +35,12 @@ Vector3 Project(const Vector3& v1, const Vector3& v2) {
 }
 
 Vector3 ClosestPoint(const Vector3& point, const Segment& segment) {
-	
-	
+
+
 	Vector3 closestPoint;
 	Vector3 project = Project(Vector3Subtract(point, segment.origin), segment.diff);
-	closestPoint = Vector3Add(segment.origin,project);
+	closestPoint = Vector3Add(segment.origin, project);
+	return closestPoint;
 }
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -48,17 +50,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	const int kWindowWidth = 1280;
+	const int kWindowHeight = 720;
+
+	Vector3 cameraTranslate = { 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate = { 0.26f,0,0 };
 
 	Segment segment_{ {-2.0f,-1.0f,0.0f},{3.0f,2.0f,2.0f} };
 	Vector3 point_{ -1.5f,0.6f,0.6f };
 	Vector3 project_ = Project(Vector3Subtract(point_, segment_.origin), segment_.diff);
 	Vector3 closestPoint_ = ClosestPoint(point_, segment_);
 
-	//点の描画
-	Sphere pointSphere_{ point_,0.01f };
-	Sphere closestPointSphere_{ closestPoint_,0.01f };
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -73,7 +79,35 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		
+		//各種行列の計算
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight),
+			0.1f, 100.0f);
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		//点の描画
+
+		Sphere pointSphere_{ point_,0.01f };
+		Sphere closestPointSphere_{ closestPoint_,0.01f };
+
+		//線分の計算
+		Vector3 start = Transform(Transform(segment_.origin, viewProjectionMatrix), viewportMatrix);
+		Vector3 end = Transform(Transform(Vector3Add(segment_.origin, segment_.diff), viewProjectionMatrix), viewportMatrix);
+
+		ImGui::Begin("Window");
+		//カメラ
+		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
+		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+
+		ImGui::DragFloat3("Point", &point_.x, 0.01f);
+		ImGui::DragFloat3("Segment oeigin", &segment_.origin.x, 0.01f);
+		ImGui::DragFloat3("segment diff", &segment_.diff.x, 0.01f);
+		ImGui::InputFloat3("Project", &project_.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
+		ImGui::End();
+
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -81,7 +115,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		//DrawSphere(pointSphere_,)
+		//グリッド線の描画
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
+		//点の描画
+		DrawSphere(pointSphere_, viewProjectionMatrix, viewportMatrix, RED);
+		DrawSphere(closestPointSphere_, viewProjectionMatrix, viewportMatrix, BLACK);
+
+		//線分の描画
+		Novice::DrawLine(
+			static_cast<int>(start.x),
+			static_cast<int>(start.y),
+			static_cast<int>(end.x),
+			static_cast<int>(end.y),
+			WHITE
+
+		);
+
 		///
 		/// ↑描画処理ここまで
 		///
