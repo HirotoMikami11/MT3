b9167bad31cd7ba9bac57b6f611cbee1642ccf66
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2024-05-10 13:47:51 +0900

    一時保存

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 #include <ImGuiManager.h>
 #include <Camera.h>
 
-const char kWindowTitle[] = "LE1A_16_ミカミ_ヒロト_MT3_02_03";
+const char kWindowTitle[] = "LE1A_16_ミカミ_ヒロト_MT3_02_04";
 
 bool IsCollision(const Segment& segment, const Plane& plane) {
 	//segmentのo=origin,b=diff
-	
+
 	//衝突しているかどうか
 	bool isCollision = false;
 
@@ -28,6 +28,53 @@ bool IsCollision(const Segment& segment, const Plane& plane) {
 
 }
 
+bool IsCollision(const Segment& segment, const Triangle& triangle) {
+
+
+}
+/// <summary>
+/// 三角形
+/// </summary>
+struct Triangle {
+
+	Vector3 vertices[3];//!<頂点
+
+};
+
+
+/// <summary>
+/// 三角形を描画
+/// </summary>
+/// <param name="triangle"></param>
+/// <param name="viewProjectionMatrix"></param>
+/// <param name="viewportMatrix"></param>
+/// <param name="color"></param>
+void DrawTriangle(const Triangle& triangle, const Matrix4x4& viewProjectionMatrix,
+	const Matrix4x4& viewportMatrix, uint32_t color) {
+	Matrix4x4 worldMatrix = MakeAffineMatrix({ 1,1,1 }, { 0,0,0 }, { 0,0,0 });
+	Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, viewProjectionMatrix);
+	Vector3 ndcPos[3];
+	Vector3 screenPos[3];
+
+	//スクリーン座標系まで変換をかける
+	for (int i = 0; i < 3; i++) {
+		ndcPos[i] = Transform(triangle.vertices[i], worldViewProjectionMatrix);
+		screenPos[i] = Transform(ndcPos[i], viewportMatrix);
+	}
+
+	Novice::DrawTriangle(
+		static_cast<int>(screenPos[0].x),
+		static_cast<int>(screenPos[0].y),
+		static_cast<int>(screenPos[1].x),
+		static_cast<int>(screenPos[1].y),
+		static_cast<int>(screenPos[2].x),
+		static_cast<int>(screenPos[2].y),
+		color,kFillModeWireFrame);
+	
+
+}
+
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -43,8 +90,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Camera* camera_ = new Camera(kWindowWidth, kWindowHeight);
 
 	Segment segment_ = { {-0.45f,0.7f,0.0f},{1.0f,0.5f,0.0f} };
-	Plane plane_ = { {0,1,0},1.0f };
-
+	Triangle triangle_ = {
+		{{-1,0,0}
+		,{0,1,0}
+		,{1,0,0}
+		}
+	};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -67,8 +118,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		ImGui::Begin("object");
 		////カメラ
-		ImGui::DragFloat3("plane_normal", &plane_.normal.x, 0.01f);
-		ImGui::DragFloat("plane_distance", &plane_.distance, 0.01f);
+		//ImGui::DragFloat3("plane_normal", &plane_.normal.x, 0.01f);
+		//ImGui::DragFloat("plane_distance", &plane_.distance, 0.01f);
 		ImGui::DragFloat3("segment_.origin", &segment_.origin.x, 0.01f);
 		ImGui::DragFloat3("segment_.diff", &segment_.diff.x, 0.01f);
 
@@ -88,9 +139,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//グリッド線の描画
 		DrawGrid(camera_->GetViewProjectionMatrix(), camera_->GetViewportMatrix());
 		//ライン
-		DrawLine(segment_, camera_->GetViewProjectionMatrix(), camera_->GetViewportMatrix(), IsCollision(segment_, plane_) ? RED : WHITE);
-		//スフィアの描画
-		DrawPlane(plane_, camera_->GetViewProjectionMatrix(), camera_->GetViewportMatrix(), WHITE);
+		//DrawLine(segment_, camera_->GetViewProjectionMatrix(), camera_->GetViewportMatrix(), IsCollision(segment_, plane_) ? RED : WHITE);
+		//三角形の描画
+		DrawTriangle(triangle_, camera_->GetViewProjectionMatrix(), camera_->GetViewportMatrix(), WHITE);
 
 		///
 		/// ↑描画処理ここまで
